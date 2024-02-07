968fc699dd7f77680da9156469a96469d8a98602
Tom Johnston <thomas.johnston@softwire.com>
2024-02-07 17:11:50 +0000

    fix nodemon in containers

--- loan-servicing-api/src/index.ts
+++ loan-servicing-api/src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-    res.send("I have come from the API");
+    res.send("I have come from the API!!");
 });
 
 app.listen(port, () => {
