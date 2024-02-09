98f1aad7a4b5e5ae82df9646b47a42a2697b5dc3
Tom Johnston <thomas.johnston@softwire.com>
2024-02-09 16:54:12 +0000

    fix line width

--- loan-servicing-ui/src/api/base-client.ts
+++ loan-servicing-ui/src/api/base-client.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-export const tryGetApiData = async <T extends object>(route: string): Promise<T | null> => {
+export const tryGetApiData = async <T extends object>(
+  route: string,
+): Promise<T | null> => {
   try {
     const response = await axios.get(`${process.env.API_URL}/${route}`)
     return response.data as T
