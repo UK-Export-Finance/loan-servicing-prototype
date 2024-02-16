a3bff800b61ef9b2bfd7777b220d3972f51d58fd
Tom Johnston <thomas.johnston@softwire.com>
2024-02-16 15:11:10 +0000

    add govuk css

--- packages/loan-servicing-ui/src/app.ts
+++ packages/loan-servicing-ui/src/app.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { NestFactory } from '@nestjs/core'
 import AppModule from 'modules/app.module'
+import { join } from 'path'
 import setupLiveReload from './server-setup/livereload'
 import configureNunjucks from './server-setup/configure-nunjucks'
 
@@ -16,6 +17,7 @@ if (process.env.NODE_ENV === 'development') {
 const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   configureNunjucks(app)
+  app.useStaticAssets(join(__dirname,'..','public'), {prefix: '/assets/'})
   await app.listen(port ?? 3000)
 }
 
--- packages/loan-servicing-ui/src/modules/app.module.ts
+++ packages/loan-servicing-ui/src/modules/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common'
 import HomeModule from './home.module'
 
-@Module({ imports: [HomeModule] })
+@Module({
+  imports: [
+    HomeModule
+  ],
+})
 class AppModule {}
 
 export default AppModule
