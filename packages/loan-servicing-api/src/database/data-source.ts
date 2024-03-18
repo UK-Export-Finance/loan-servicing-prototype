fc4ca1106488421e4d7280620e56ddb011ea7f71
Tom Johnston <thomas.johnston@softwire.com>
2024-03-18 12:30:09 +0000

    alt monorepo approach

--- packages/loan-servicing-api/src/database/data-source.ts
+++ packages/loan-servicing-api/src/database/data-source.ts
@@ -10,8 +10,8 @@ const SQL_DB_CONFIG: TypeOrmModuleOptions = {
   type: 'mssql',
   host: process.env.SQL_DB_HOST,
   port: Number(process.env.SQL_DB_PORT),
-  username: 'sa',
-  password: 'AbC!2345',
+  username: process.env.SQL_DB_USER,
+  password: process.env.SQL_DB_PASSWORD,
   database: 'LoanServicing',
   synchronize: true,
   entities: [
