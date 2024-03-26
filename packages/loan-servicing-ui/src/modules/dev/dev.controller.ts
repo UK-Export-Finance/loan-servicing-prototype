55b9e8a0267ba5baa7b7d566a6a604faa3edb833
Tom Johnston <thomas.johnston@softwire.com>
2024-03-26 11:12:03 +0000

    add button to frontend to trigger update functions

--- packages/loan-servicing-ui/src/modules/dev/dev.controller.ts
+++ packages/loan-servicing-ui/src/modules/dev/dev.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Query, Req, Res } from '@nestjs/common'
+import { Controller, Get, Post, Query, Req, Res } from '@nestjs/common'
 import { putApiData } from 'api/base-client'
+import axios from 'axios'
 import { Response, Request } from 'express'
 
 @Controller('dev')
@@ -11,6 +12,18 @@ class DevController {
     @Res() response: Response,
   ): Promise<void> {
     await putApiData(`system/date/${new Date(dateStr).toISOString()}`, {})
+    const sourcePage = request.headers.referer
+    response.redirect(sourcePage || '/')
+  }
+
+  @Post('systemDate/next')
+  async moveToNextDay(
+    @Req() request: Request,
+    @Res() response: Response,
+  ): Promise<void> {
+    await axios.post(
+      `${process.env.FUNCTIONS_URL}/api/triggerActiveFacilityUpdate`,
+    )
     const sourcePage = request.headers.referer
     response.redirect(sourcePage || '/')
   }
