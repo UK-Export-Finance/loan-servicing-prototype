cc79cb868a21d67311ff45525d70e30dc57c57c7
Tom Johnston <thomas.johnston@softwire.com>
2024-05-16 14:58:11 +0100

    tidy tests

---
+++ packages/loan-servicing-api/src/modules/server/server.test.ts
@@ -0,0 +1,18 @@
+import ServerController from 'modules/server/server.controller'
+import { Test } from '@nestjs/testing'
+
+describe('Server controller', () => {
+  let serverController: ServerController
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [ServerController],
+    }).compile()
+
+    serverController = moduleRef.get(ServerController)
+  })
+
+  it('Health check endpoint returns expected response', () => {
+    expect(serverController.getFacility()).toEqual('LS Healthy')
+  })
+})
--- packages/loan-servicing-api/test/unit/modules/server/server.controller.test.ts
+++
@@ -1,7 +0,0 @@
-import ServerController from "modules/server/server.controller"
-
-test('Health check endpoint returns expected response', () => {
-  const serverController = new ServerController()
-
-  expect(serverController.getFacility()).toEqual('LS Healthy')
-})
