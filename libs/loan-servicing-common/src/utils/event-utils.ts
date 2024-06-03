fcd0fbe4e0f6c8aba05e6d264b1f1513dc75a923
Tom Johnston <thomas.johnston@softwire.com>
2024-06-03 12:34:41 +0100

    fix docker images

--- libs/loan-servicing-common/src/utils/event-utils.ts
+++ libs/loan-servicing-common/src/utils/event-utils.ts
@@ -8,6 +8,5 @@ export const sortByDateOnKey =
   (a: T, b: T): number =>
     (a[key] as Date).getTime() - (b[key] as Date).getTime()
 
-// eslint-disable-next-line import/prefer-default-export
 export const sortEventByEffectiveDate =
   sortByDateOnKey<HasEffectiveDate>('effectiveDate')
